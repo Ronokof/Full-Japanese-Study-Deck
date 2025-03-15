7b46479ae9ef22af54b63a86fe40cee8d64c1e70
Ronokof <ronokof@outlook.com>
2025-03-15 17:15:11 +0200

    Added notetype and deck columns to notes

--- src/utils.ts
+++ src/utils.ts
@@ -152,12 +152,13 @@ export function saveEntries(resultPath: string): void {
 
         for (let jsonFile of jsonFiles) {
             let jsonFileContent: Result[] | null | undefined = JSON.parse(readFileSync(`${jsonDir}/${jsonFile}`, 'utf-8'));
+            let jsonFilename: string = path.parse(jsonFile).name;
 
             if (jsonFileContent && Array.isArray(jsonFileContent) && jsonFileContent.every((result: Result) => typeof result === 'object')) {
-                let ankiNotesFile: string | undefined = generateAnkiNotesFile(jsonFileContent);
+                let ankiNotesFile: string | undefined = generateAnkiNotesFile(jsonFileContent, jsonFilename);
 
                 if (ankiNotesFile) {
-                    writeFileSync(`${resultPath}/${path.parse(jsonFile).name}.txt`, ankiNotesFile, 'utf-8');
+                    writeFileSync(`${resultPath}/${jsonFilename}.txt`, ankiNotesFile, 'utf-8');
                     console.log(`Saved ${jsonFile} with ${jsonFileContent.length} entries`);
                 }
             }
@@ -165,14 +166,32 @@ export function saveEntries(resultPath: string): void {
     }
 }
 
+export function isWord(entry: Result): entry is Word {
+    return ((entry as Word).translations !== undefined && (entry as Word).readings !== undefined);
+}
+
+export function isRadical(entry: Result): entry is Radical {
+    return ((entry as Radical).radical !== undefined && (entry as Radical).reading !== undefined && (entry as Radical).meanings !== undefined);
+}
+
+export function isKanji(entry: Result): entry is Kanji {
+    return ((entry as Word).translations === undefined && (entry as Word).readings === undefined && (entry as Radical).radical === undefined && (entry as Kanji).kanji !== undefined);
+}
+
+export function isKana(entry: Result): entry is Kana {
+    return ((entry as Kana).kana !== undefined && (entry as Kana).reading !== undefined);
+}
+
+export function isGrammar(entry: Result): entry is Grammar {
+    return ((entry as Grammar).point !== undefined && (entry as Grammar).meaning !== undefined);
+}
+
 export const createNotes: (notes: string[]) => string = (notes: string[]) => `<ul class="note-list">${notes.filter((note: string) => !note.trim().startsWith('See also:')).map((note: string) => `<li class="note">${note.trim()}</li>`).join('')}</ul>`;
 export const createEntry: (entry: string, notes?: string[] | undefined) => string = (entry: string, notes?: string[] | undefined) => `<div class="entry">${entry}${(notes && notes.length > 0) ? createNotes(notes) : ''}</div>`;
 export const noKanjiForms: string = '<span class="word word-kanjiform">(no kanji forms)</span>';
 
 export function generateAnkiNote(entry: Result): Note {
-    if ((entry as Word).translations !== undefined && (entry as Word).readings !== undefined) {
-        entry = entry as Word;
-
+    if (isWord(entry)) {
         let kanjiFormsField: string = (entry.kanjiForms) ? entry.kanjiForms.map((kanjiFormEntry: KanjiForm, index: number) => (index === 0) ? createEntry(`<span class="word word-kanjiform"><ruby><rb>${kanjiFormEntry.kanjiForm}</rb><rt>${(entry as Word).readings[0]!.reading}</rt></ruby>${(kanjiFormEntry.audio !== undefined) ? `<br>[sound:${kanjiFormEntry.audio}]` : ''}</span>`, kanjiFormEntry.notes) : createEntry(`<details><summary>Show</summary><span class="word word-kanjiform">${kanjiFormEntry.kanjiForm}${(kanjiFormEntry.audio !== undefined) ? `<br>[sound:${kanjiFormEntry.audio}]` : ''}</span></details>`, kanjiFormEntry.notes)).join('') : noKanjiForms;
         let readingsField: string = entry.readings.map((readingEntry: Reading, index: number) => createEntry(`${(index > 0) ? '<details><summary>Show</summary>' : ''}<span class="word word-reading">${readingEntry.reading}${(readingEntry.audio !== undefined) ? `<br>[sound:${readingEntry.audio}]` : ''}</span>${(index > 0) ? '</details>' : ''}`, readingEntry.notes)).join('');
         let translationsField: string = entry.translations.map((translationEntry: Translation) => createEntry(`<span class="word word-translation">${translationEntry.translation}</span>`, translationEntry.notes)).join('');
@@ -187,9 +206,7 @@ export function generateAnkiNote(entry: Result): Note {
         return { note: `${(kanjiFormsField !== noKanjiForms && !usuallyInKana) ? kanjiFormsField : readingsField}\t${(kanjiFormsField !== noKanjiForms && !usuallyInKana) ? readingsField : kanjiFormsField}\t${translationsField}\t${kanjiField}\t${phrasesField}\t${noteIDField}${(tagsField) ? `\t${tagsField}` : ''}`.replaceAll('\n', '<br>'), fieldNumber: 7 };
     }
 
-    if ((entry as Radical).radical !== undefined && (entry as Radical).reading !== undefined && (entry as Radical).meanings !== undefined) {
-        entry = entry as Radical;
-
+    if (isRadical(entry)) {
         let radicalField: string = createEntry(`<span class="radical radical-character">${entry.radical}</span>`);
         let readingField: string = createEntry(`<span class="radical radical-reading">${entry.reading}</span>`);
         let meaningsField: string = entry.meanings.map((meaningEntry: string) => createEntry(`<span class="radical radical-meaning">${meaningEntry}</span>`)).join('');
@@ -203,9 +220,7 @@ export function generateAnkiNote(entry: Result): Note {
         return { note: `${radicalField}\t${readingField}\t${meaningsField}\t${mnemonicField}\t${kanjiField}\t${strokesField}\t${noteIDField}${(tagsField) ? `\t${tagsField}` : ''}`.replaceAll('\n', '<br>'), fieldNumber: 8 };
     }
 
-    if ((entry as Kanji).kanji !== undefined) {
-        entry = entry as Kanji;
-
+    if (isKanji(entry)) {
         let kanjiField: string = createEntry(`<span class="kanji kanji-character">${entry.kanji}</span>`);
         let meaningsField: string = (entry.meanings) ? entry.meanings.map((meaningEntry: string) => createEntry(`<span class="kanji kanji-meaning">${meaningEntry}</span>`)).join('') : '<span class="kanji kanji-meaning">(no meanings)</span>';
         let onyomiField: string = (entry.onyomi) ? entry.onyomi.map((onyomiEntry: string) => createEntry(`<span class="kanji kanji-onyomi">${onyomiEntry}</span>`)).join('') : '<span class="kanji kanji-onyomi">(no onyomi) (kokuji)</span>';
@@ -221,9 +236,7 @@ export function generateAnkiNote(entry: Result): Note {
         return { note: `${kanjiField}\t${meaningsField}\t${onyomiField}\t${kunyomiField}\t${componentsField}\t${mnemonicField}\t${wordsField}\t${strokesField}\t${noteIDField}${(tagsField) ? `\t${tagsField}` : ''}`.replaceAll('\n', '<br>'), fieldNumber: 10 };
     }
 
-    if ((entry as Kana).kana !== undefined && (entry as Kana).reading !== undefined) {
-        entry = entry as Kana;
-
+    if (isKana(entry)) {
         let kanaField: string = createEntry(`<span class="kana kana-character">${entry.kana}</span>`);
         let readingField: string = createEntry(`<span class="kana kana-reading">${entry.reading}${(entry.audio !== undefined) ? `<br>[sound:${entry.audio}]` : ''}<br>${(entry.svg) ? `<img class="kana kana-stroke-order" src="${entry.svg}" alt="${entry.kana} stroke order SVG">` : '(no stroke order SVG available)'}</span>`);
         let noteIDField: string | undefined = (entry.noteID) ? `<span class="kana kana-noteid">${entry.noteID}</span>` : undefined;
@@ -233,9 +246,7 @@ export function generateAnkiNote(entry: Result): Note {
         return { note: `${kanaField}\t${readingField}\t${noteIDField}${(tagsField) ? `\t${tagsField}` : ''}`.replaceAll('\n', '<br>'), fieldNumber: 4 };
     }
 
-    if ((entry as Grammar).point !== undefined && (entry as Grammar).meaning !== undefined) {
-        entry = entry as Grammar;
-
+    if (isGrammar(entry)) {
         let pointField: string = createEntry(`<span class="grammar grammar-point">${entry.point}</span>`);
         let readingField: string = (entry.readings) ? entry.readings.map((readingEntry: Reading) => createEntry(`<span class="grammar grammar-reading">${readingEntry.reading}</span>`)).join('') : '<span class="grammar grammar-reading">(no additional readings)</span>';
         let meaningField: string = createEntry(`<span class="grammar grammar-meaning">${entry.meaning.meaning}${(entry.meaning.example && entry.meaning.example.length > 0) ? `<br><span class="grammar grammar-meaning-example">${entry.meaning.example}</span>` : ''}</span>`);
@@ -251,7 +262,7 @@ export function generateAnkiNote(entry: Result): Note {
     throw new Error('Invalid entry');
 }
 
-export function generateAnkiNotesFile(list: Result[]): string | undefined {
+export function generateAnkiNotesFile(list: Result[], urlPath: Path | string): string | undefined {
     list = list.filter((result: Result) => {
         if ((result as Kanji).kanji !== undefined) if ((result as Kanji).doNotCreateNote === true) return false;
 
@@ -259,12 +270,54 @@ export function generateAnkiNotesFile(list: Result[]): string | undefined {
     });
 
     if (list.length > 0) {
-        let headers: string[] = ['#separator:tab\n', '#html:true\n'];
+        let headers: string[] = ['#separator:tab\n', '#html:true\n', '#notetype column:1\n', '#deck column:2\n'];
         let ankiNotes: string = list.map((result: Result) => {
             let note: Note = generateAnkiNote(result);
-            if (headers.length === 2) headers.push(`#tags column:${note.fieldNumber}\n`);
+            if (headers.length === 4) headers.push(`#tags column:${note.fieldNumber + 2}\n`);
 
-            return note.note;
+            let noteType: string = '';
+            let deck: string = 'Full Japanese Study Deck [JLPT N5~N1 vocab/kanji + common vocab and kanji + grammar]::';
+
+            if (isWord(result)) noteType = 'Word';
+            if (isRadical(result)) noteType = 'Radical';
+            if (isKanji(result)) noteType = 'Kanji';
+            if (isKana(result)) noteType = 'Kana';
+            if (isGrammar(result)) noteType = 'Grammar';
+
+            if (noteType.length === 0) throw new Error('Invalid entry');
+
+            let filename: string = (typeof urlPath === 'string') ? urlPath : urlPath.filename;
+
+            let filenameParts: string[] = filename.split('_');
+
+            switch (filenameParts.length) {
+                case 1:
+                    if (filenameParts[0]! === 'hiragana') deck += 'Kana::Hiragana';
+                    else if (filenameParts[0]! === 'katakana') deck += 'Kana::Katakana';
+                    else if (filenameParts[0]! === 'radicals') deck += 'Kanji radicals';
+
+                    break;
+                case 2:
+                    if (filenameParts[0]! === 'kana' && filenameParts[1]! === 'words') deck += 'Vocab with no kanji';
+                    else if (filenameParts[0]! === 'grammar') deck += `Grammar::${(filenameParts[1]! === 'additional') ? 'Additional' : filenameParts[1]!.toUpperCase()}`;
+                    else if (filenameParts[1]! === 'extended') deck += `Kana::${(filenameParts[0]! === 'hiragana') ? 'Hiragana' : (filenameParts[0]! === 'katakana') ? 'Katakana' : ''} extended`;
+                    else if (filenameParts[0]! === 'kanji' && filenameParts[1]!.startsWith('n')) deck += `JLPT::Kanji::${filenameParts[1]!.toUpperCase()}`;
+                    else if (filenameParts[0]! === 'vocab' && filenameParts[1]!.startsWith('n')) deck += `JLPT::Vocab::${filenameParts[1]!.toUpperCase()}`;
+
+                    break;
+                case 3:
+                    if (filenameParts[0]! === 'kanji' && filenameParts[1]! === 'block') deck += `All CJK kanji::Kanji::Block ${filenameParts[2]!.toUpperCase()}`;
+
+                    break;
+                case 4:
+                    if (filenameParts[0]! === 'kanji' && filenameParts[1]! === 'words' && filenameParts[2]! === 'block') deck += `All CJK kanji::Vocab::Block ${filenameParts[3]!.toUpperCase()}`;
+
+                    break;
+                default:
+                    throw new Error('Invalid filename');
+            }
+
+            return `${noteType}\t${deck}\t${note.note}`;
         }).join('\n').trim();
 
         if (ankiNotes.length === 0) throw new Error('Invalid list');
